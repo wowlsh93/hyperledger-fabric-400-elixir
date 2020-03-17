20199a55983646c0912deeb517669c3a8adaf875
wowlsh93@gmail.com <lsh7001!@#>
2020-03-17 14:36:54 +0900

    init - 3

--- fabric/test.ex
+++ fabric/test.ex
@@ -12,18 +12,19 @@ defmodule Fabric do
     spawn(__MODULE__, :endorsor, [])
   end
 
-  def add_trans(key,value) do
+  def add_trans(fabric, key, value ) do
     ref = make_ref()
-    send(key,value,{:proposal, self(), ref})
+    send(fabric,{:proposal, self(), ref, key, value})
     receive do
-      {:ok, ^ref, rwset} -> IO.puts(rwset)
+      {:ok, ^ref, rwset} -> rwset
     end
+
   end
 
   def endorsor() do
     receive do
-      {:proposal, sender, ref} ->
-        sned(sender, {:ok, ref , "rwset"})
+      {:proposal, sender, ref, key, value} ->
+        send(sender, {:ok, ref , key})
         endorsor()
     end
   end
@@ -33,25 +34,25 @@ end
 defmodule Middleware do
 
   def start(count) when is_integer(count) do
-    Fabric.start("org1")
+    fabric = Fabric.start()
     acc = 0
     IO.puts("start-1")
     start_time = System.system_time(:second)
-    loop(count, acc)
+    loop(fabric, count, acc)
     end_time = System.system_time(:second)
     IO.puts(to_string(end_time - start_time))
     IO.puts("start-2")
 
   end
 
-  def start_transaction(key, value) do
-    Fabric.add_trans(key,value)
+  def start_transaction(fabric, key, value) do
+    Fabric.add_trans(fabric, key,value)
   end
 
-  defp loop(count, acc) do
+  defp loop( fabric, count, acc) do
     if acc <= count do
-      start_transaction(to_string(acc),to_string(acc))
-      loop(count, acc+1)
+      start_transaction(fabric, to_string(acc),to_string(acc))
+      loop(fabric, count, acc+1)
     end
   end
 
